f82dcb5668edf7c168e6d56c130b0a674fff103b
Eve Reichmann <eve.reichmann@gmail.com>
2020-08-18 14:12:33 -0500

    image dropdown select

--- src/database.js
+++ src/database.js
@@ -1,25 +1,38 @@
-function main(){
-    renderUser()
-}
+//  function main(){
+//     fetchReqestsImage()
+//     fetchReqestsWrappingPaper()
+//     fetchReqestsUser()
+//  }
 
-function renderUser(){
-    const userPage = document.getElementById("index-container")
-    fetch("http://localhost:3000/users")
-        .then(resp=> resp.json())
-        .then(usersData=> {
-            usersData.forEach(user=> {
-                console.log(user)
-            })
-        })
-        fetch("http://localhost:3000/wrapping_papers")
-        .then(resp=> resp.json())
-        .then(wrappingPaperData=> {
-            wrappingPaperData.forEach(paper=> {
-                console.log(paper)
-            })
-        })
+// function fetchReqestsUser(){
+//     fetch("http://localhost:3000/users")
+//         .then(resp=> resp.json())
+//         .then(usersData=> {
+//             usersData.forEach(user=> {
+//                 console.log(user)
+//             })
+//         })
+//     }
 
-}
+// function fetchReqestsWrappingPaper(){
+//     fetch("http://localhost:3000/wrapping_papers")
+//         .then(resp=> resp.json())
+//         .then(wrappingPaperData=> {
+//             wrappingPaperData.forEach(paper=> {
+//                 console.log(paper)
+//             })
+//         })
+// }
+// function fetchReqestsImage(){
+//     fetch("http://localhost:3000/images")
+//             .then(resp=> resp.json())
+//             .then(imageData=> {
+//                 imageData.forEach(image=> {
+//                     console.log(image)
+//                 })
+//             })
+// }    
 
 
-main()
+
+// main()
